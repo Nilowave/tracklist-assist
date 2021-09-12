95ec7816fe95be20740afa146c525da223257fe1
Danilo <danilo@mediamonks.com>
2021-09-11 19:06:31 -0700

    fix: refactor hard coded connection string to Heroku config var

--- db/index.js
+++ db/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/tracklist', { useNewUrlParser: true }).catch((e) => {
+const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/tracklist';
+
+mongoose.connect(uri, { useNewUrlParser: true }).catch((e) => {
   console.error('Connection error', e.message);
 });
 
