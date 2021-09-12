0a98c2edd00159f715241ed1e360b9e6b2a76433
Danilo <danilo@mediamonks.com>
2021-09-11 23:06:37 -0700

    added socket io to auto update FE

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const app = express();
+
+const http = require('http');
+const server = http.createServer(app);
 const cors = require('cors');
+const { Server } = require('socket.io');
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
 
 const db = require('./db');
 const itemRouter = require('./src/routes/item-router');
 
-const app = express();
 const apiPort = process.env.PORT || 1337;
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,10 +25,18 @@ app.use(bodyParser.json());
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
 app.get('/', (req, res) => {
   res.send('Tracklist');
 });
 
 app.use('/api', itemRouter);
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
+app.post('/api/item', (req, res, next) => {
+  io.sockets.emit('message', { id: 'update', data: req.body });
+});
+
+server.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
