7b5e5ab8c26de8e3e365e621a1c5dabc4c35c492
Danilo <danilo@mediamonks.com>
2021-09-18 15:43:10 -0700

    update item routes

--- backend/src/routes/item-router.js
+++ backend/src/routes/item-router.js
@@ -6,10 +6,11 @@ const UserCtrl = require('../controllers/user-ctrl');
 module.exports = (io) => {
   const router = express.Router();
 
+  router.post('/assistant', (req, res) => ItemCtrl.upsert(req, res, io));
   router.post('/item', (req, res) => ItemCtrl.upsert(req, res, io));
-  router.put('/item/:id', (req, res) => ItemCtrl.updateItem(req, res, io));
+  // router.put('/item/:id', (req, res) => ItemCtrl.updateItem(req, res, io));
   router.delete('/item/:id', (req, res) => ItemCtrl.deleteItem(req, res, io));
-  router.get('/item/:name', ItemCtrl.getItemByName);
+  // router.get('/item/:name', ItemCtrl.getItemByName);
   router.get('/items', (req, res) => ItemCtrl.getItems(req, res, io));
 
   // User
